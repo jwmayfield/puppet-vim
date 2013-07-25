b5c858f430f542e6878346903d3e0e3d0c1e902b
Jason Mayfield <jwmayfield@gmail.com>
2013-07-25 14:54:05 -0400

    Break out vim install into its own thing

---
+++ manifests/install.pp
@@ -0,0 +1,16 @@
+# Installs vim from homebrew
+define vim::install {
+  package { 'vim':
+    require => Package['mercurial']
+  }
+  # Install mercurial since the vim brew package don't satisfy the requirement (vim is fetched using $ hg)
+  package { 'mercurial':
+    require => Package['docutils']
+  }
+  # docutils is required by mercurial.
+  # https://github.com/boxen/puppet-vim/issues/12
+  package { 'docutils':
+    ensure   => installed,
+    provider => pip,
+  }
+}
---
+++ manifests/pathogen.pp
@@ -0,0 +1,31 @@
+# Installs vim-pathogen
+
+# Examples
+#
+#   include vim
+#   vim::pathogen
+
+define vim::pathogen {
+  require vim
+
+  file { [$vimdir,
+    "${vimdir}/autoload",
+    "${vimdir}/bundle"]:
+    ensure  => directory,
+    recurse => true,
+  }
+
+  repository { "${vimdir}/vim-pathogen":
+    source => 'tpope/vim-pathogen'
+  }
+
+  file { "${vimdir}/autoload/pathogen.vim":
+    target  => "${vimdir}/vim-pathogen/autoload/pathogen.vim",
+    require => [
+      File[$vimdir],
+      File["${vimdir}/autoload"],
+      File["${vimdir}/bundle"],
+      Repository["${vimdir}/vim-pathogen"]
+    ]
+  }
+}
